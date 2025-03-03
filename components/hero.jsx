c96f4d22317261aff255d83d40a31644a6c3acfc
Anjany Pandey <anjany.pandey06@gmail.com>
2025-03-03 23:04:03 +0530

    gradient is not wokring on that

---
+++ components/hero.jsx
@@ -0,0 +1,49 @@
+import Link from "next/link";
+import React from "react";
+import { Button } from "./ui/button";
+import Image from "next/image";
+
+const HeroSection = () => {
+  return (
+    <section className="w-full pt-36 md:pt-48 pb-10">
+      <div className="space-y-6 text-center">
+        <div className="space-y-6 mx-auto">
+          <h1 className="text-5xl font-bold md:text-6xl lg:text-7xl xl:text-8xl gradient-title">
+            Your AI Career Coach for
+            <br />
+            Professional Success
+          </h1>
+          <p>
+            Advance your career with personalized guidance, interview prep, and
+            AI- powered tools for job success.
+          </p>
+        </div>
+        <div>
+          <Link href="/dashboard">
+            <Button size="lg" className="px-8">
+              Get Started
+            </Button>
+          </Link>
+          <Link href="/https:www.youtube.com">
+            <Button size="lg" className="px-8" variant="outline">
+              Get Started
+            </Button>
+          </Link>
+        </div>
+        <div>
+          <div>
+            <Image
+              src={"/banner.jpeg"}
+              width={1280}
+              height={720}
+              alt="Banner"
+              className="rounded-lg shadow-2xl border mx-auto"
+              priority
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+export default HeroSection;

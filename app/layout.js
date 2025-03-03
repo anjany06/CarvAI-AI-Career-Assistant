25dcaf8ebe915339f0a3bcfc6c1211582473d850
Anjany Pandey <anjany.pandey06@gmail.com>
2025-03-03 21:46:16 +0530

    setupped inngest

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import Header from "@/components/header";
 import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,7 +14,11 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+      }}
+    >
       <html lang="en" suppressHydrationWarning>
         <body className={`${inter.className} `}>
           <ThemeProvider
@@ -26,6 +31,7 @@ export default function RootLayout({ children }) {
             <Header />
 
             <main className="min-h-screen">{children}</main>
+
             {/* footer */}
             <footer className="bg-muted/50 py-12">
               <div className="container mx-auto px-4 text-center text-gray-200">
--- components/header.jsx
+++ components/header.jsx
@@ -20,7 +20,7 @@ import Image from "next/image";
 
 export default async function Header() {
   return (
-    <header className="fixed top-0 w-full border-b bg-background/80 backdrop-blur-md z-50 supports-[backdrop-filter]:bg-background/60">
+    <header className="fixed top-0 min-w-screen border-b bg-background/80 backdrop-blur-md z-50 supports-[backdrop-filter]:bg-background/60">
       <nav className="container mx-auto px-4 h-16 flex items-center justify-between">
         <Link href="/">
           <Image

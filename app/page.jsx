b5eb88782c3464a809a3995f603987c0e80babed
Anjany Pandey <anjany.pandey06@gmail.com>
2025-03-05 21:54:18 +0530

    UI: Features section

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,55 @@
 import HeroSection from "@/components/hero";
+import { Card, CardContent } from "@/components/ui/card";
+import { features } from "@/data/features";
 
 export default function Home() {
   return (
     <div>
       <div className="grid-background"></div>
       <HeroSection />
+
+      {/* features section */}
+
+      <section className="w-full py-12 md:py-24 lg:py-32 bg-background">
+        <div className="container mx-auto px-4 md:px-6">
+          <h2 className="text-3xl font-bold tracking-tighter text-center mb-12">
+            Powerful Features for Your Career Growth
+          </h2>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 max-w-6xl mx-auto">
+            {features.map((feature, index) => {
+              return (
+                <Card
+                  key={index}
+                  className="border-2 hover:border-primary transition-colors duration-300"
+                >
+                  <CardContent className="pt-6 text-center flex flex-col items-center">
+                    <div className="flex flex-col items-center justify-center">
+                      {feature.icon}
+                    </div>
+                    <h1 className="text-xl font-bold mb-2">{feature.title}</h1>
+                    <p className="text-muted-foreground">
+                      {feature.description}
+                    </p>
+                  </CardContent>
+                </Card>
+              );
+            })}
+          </div>
+        </div>
+      </section>
+
+      {/* <section className=" w-full py-12 md:py-24 bg-muted/50">
+        <div className="container mx-auto px-4 md:px-6">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 max-w-6xl mx-auto">
+
+            <div className="flex flex-col items-center justify-center space-y-2">
+              <h3 className="text-4xl font-bold">50+</h3>
+              <p></p>
+            </div>
+
+          </div>
+        </div>
+      </section> */}
     </div>
   );
 }

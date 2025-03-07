90a60c9bc4fbbc0dbcec34b5a0179ce290e8f587
Anjany Pandey <anjany.pandey06@gmail.com>
2025-03-07 22:05:07 +0530

    Feat : Onboarding form done

--- app/(main)/onboarding/page.jsx
+++ app/(main)/onboarding/page.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import OnBoardingForm from "./_components/onboarding-form";
-import { industries } from "@/data/industries";
 import { getUserOnboardingStatus } from "@/actions/user";
 import { redirect } from "next/navigation";
+import { industries } from "@/data/industries";
 
 const OnBoarding = async () => {
   // Check if user is already onboarded
@@ -11,12 +11,11 @@ const OnBoarding = async () => {
   if (isOnboarded) {
     redirect("/dashboard");
   }
-
   return (
     // so now why sep component as we can not use client in this as this is server
     // comp as we making api call to server to check is user onboarded as why a sep comp
     <main>
-      <OnBoardingForm industry={industries} />
+      <OnBoardingForm industries={industries} />
     </main>
   );
 };

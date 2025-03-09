fd45b2f63316030662d3d27133770771e89a18b2
Anjany Pandey <anjany.pandey06@gmail.com>
2025-03-10 01:46:43 +0530

    `Refactor PerformanceChart component to use recharts and add chart functionality`

--- app/(main)/interview/page.jsx
+++ app/(main)/interview/page.jsx
@@ -12,7 +12,7 @@ const Interview = async () => {
         Interview Preparation
       </h1>
 
-      <div>
+      <div className="space-y-6">
         <StatsCards assessments={assessments} />
         <PerformanceChart assessments={assessments} />
         <QuizList assessments={assessments} />

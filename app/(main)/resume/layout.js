3ad37331614eb74317a0400de0b401071131ce2d
Anjany Pandey <anjany.pandey06@gmail.com>
2025-03-11 02:30:56 +0530

    `feat: updated resume builder component with new features and schema validation`

---
+++ app/(main)/resume/layout.js
@@ -0,0 +1,16 @@
+import React, { Suspense } from "react";
+import { HashLoader } from "react-spinners";
+
+const Layout = ({ children }) => {
+  return (
+    <div className="px-5">
+      <Suspense
+        fallback={<HashLoader className="mt-4" width={"100%"} color="gray" />}
+      >
+        {children}
+      </Suspense>
+    </div>
+  );
+};
+
+export default Layout;
--- app/(main)/resume/page.jsx
+++ app/(main)/resume/page.jsx
@@ -1,11 +1,12 @@
 import { getResume } from "@/actions/resume";
 import React from "react";
+import ResumeBuilder from "./_components/resume-builder";
 
 const ResumePage = async () => {
   const resume = await getResume();
   return (
-    <div>
-      <ResumeBuilder />
+    <div className="container mx-auto p-6">
+      <ResumeBuilder initialContent={resume?.content} />
     </div>
   );
 };

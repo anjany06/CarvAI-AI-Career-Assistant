043ecfe87a0936e9f76c5e4b5c5771267d7d942e
Anjany Pandey <anjany.pandey06@gmail.com>
2025-03-24 14:43:50 +0530

    `Refactor MainLayout component to add responsive padding`

--- app/(main)/layout.js
+++ app/(main)/layout.js
@@ -1,9 +1,9 @@
 import React from "react";
 
 const MainLayout = ({ children }) => {
-  // if user is not onboarded Redirect to OnBoarding..
-
-  return <div className='container mx-auto mt-24 mb-20"'>{children}</div>;
+  return (
+    <div className='container mx-auto md:px-8 mt-24 mb-20"'>{children}</div>
+  );
 };
 
 export default MainLayout;

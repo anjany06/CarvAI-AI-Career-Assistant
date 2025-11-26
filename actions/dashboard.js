a874af9d6f108b98aa7f62d2e50334245b59a1ef
Anjany Pandey <anjany.pandey06@gmail.com>
2025-11-26 17:37:18 +0530

    Fix the assessment

--- actions/dashboard.js
+++ actions/dashboard.js
@@ -4,7 +4,7 @@ import { db } from "@/lib/prisma";
 import { auth } from "@clerk/nextjs/server";
 import { GoogleGenerativeAI } from "@google/generative-ai";
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-const model = genAI.getGenerativeModel({model: "gemini-2.5-flash",
+const model = genAI.getGenerativeModel({model: "gemini-2.0-flash",
 });
 export const generateAIInsights = async (industry) => {
   const prompt = `
--- actions/interview.js
+++ actions/interview.js
@@ -6,7 +6,7 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 const model = genAI.getGenerativeModel({
-  model: "gemini-2.5-flash",
+  model: "gemini-2.0-flash",
 });
 export async function generateQuiz() {
   const { userId } = await auth();
@@ -22,7 +22,7 @@ export async function generateQuiz() {
 
   try {
     const prompt = `
-    Generate 5 technical interview questions for a ${
+    Generate 10 technical interview questions for a ${
       user.industry
     } professional${
       user.skills?.length ? ` with expertise in ${user.skills.join(", ")}` : ""

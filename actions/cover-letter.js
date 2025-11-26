5200111d9a022a8154000453de9c27c3dcbaa559
Anjany Pandey <anjany.pandey06@gmail.com>
2025-11-26 17:56:32 +0530

    Changed the model

--- actions/cover-letter.js
+++ actions/cover-letter.js
@@ -5,7 +5,7 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 const model = genAI.getGenerativeModel({
-  model: "gemini-2.5-flash",
+  model: "gemini-2.0-flash",
 });
 
 export async function generateLetter(data) {
